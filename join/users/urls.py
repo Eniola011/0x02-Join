96823ecef1f44131305ac7ab3185ab9255f48ef8
Eniola011 <yettyagbalu@gmail.com>
2024-06-17 17:34:00 +0100

    urls.py

--- join/users/urls.py
+++ join/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import homepage, CustomLoginView, RegisterView, CustomPasswordResetView
+from django.contrib.auth import views as auth_views
+from .views import homepage, CustomLoginView, RegisterView, CustomPasswordResetView, ActivateView
 
 urlpatterns = [
     path('', homepage, name='home'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('reset_password/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('activate/<uidb64>/<token>/', ActivateView.as_view(), name='activate'),
 ]
